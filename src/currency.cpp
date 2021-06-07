f212e1da2b4140e453ed340277b13f69344c1d33
Jeremy Goossen <jeremygoossen@letu.edu>
2021-06-06 20:30:11 -0500

    Adds a new Currency class to save and print dollar amounts.

---
+++ src/currency.cpp
@@ -0,0 +1,67 @@
+#include "currency.h"
+
+#include <cstdio>
+#include <cmath>
+#include <cassert>
+
+#include "ansi.h"
+
+float Currency::dollars(void) const {
+	return (float)cents/100;
+}
+
+std::string Currency::display(const int &max_characters) const {
+	if (cents < 0) {
+		// Format negative number
+		int characters = max_characters;
+		switch (negativeFormat_) {
+			case NEGATIVE_SIGN :
+				characters--; 		// Reserve space for negative sign
+				break;
+			case PARENTHESIS :
+				characters -= 2; 	// Reserve space for parenthesis
+				break;
+			case RED_COLOR :
+			default :
+				break;
+		}
+		return format_negative_string(display_magnitude(characters));
+	} else {
+		// Format positive number
+		return display_magnitude(max_characters);
+	}
+}
+
+std::string Currency::display_magnitude(const int &max_chars) const {
+	constexpr int buffer_len = 16;
+	assert(buffer_len > max_chars + 1); // Ensure buffer can fit all characters and null terminator
+	char display_value[buffer_len];
+	int chars = snprintf( display_value, buffer_len, "$%04.2f", (float)abs(cents)/100);
+	return (chars <= max_chars)
+				? std::string(display_value)
+				: std::string(max_chars, '*');	
+}
+
+std::string Currency::format_negative_string(std::string positiveString) const {
+	std::string negativeString;
+	
+	switch (negativeFormat_) {
+		case NEGATIVE_SIGN :
+			negativeString = "-" + positiveString;
+			break;
+		case PARENTHESIS :
+			negativeString = "(" + positiveString + ")";
+			break;
+		case RED_COLOR :
+			negativeString = ansi::SAVE_ATTRIBUTES + ansi::RED + positiveString + ansi::RESTORE_ATTRIBUTES;
+			break;
+		default :
+			negativeString = "-" + positiveString;
+	}
+	
+	return negativeString;
+}
+
+bool Currency::operator<(const Currency &rhs) const {
+	return this->cents < rhs.cents;
+}
---
+++ src/currency.h
@@ -0,0 +1,34 @@
+#ifndef CURRENCY_H_
+#define CURRENCY_H_
+
+#include <string>
+
+/// Allowable formats for negative currencies
+enum NegativeFormat{
+	NEGATIVE_SIGN, 	/// Value printed to the right of a negative sign
+	PARENTHESIS,	/// Value printed inside parenthesis	
+	RED_COLOR 		/// Value printed in red
+};
+
+class Currency{
+  public:
+	Currency(NegativeFormat negativeFormat = NEGATIVE_SIGN)
+		: negativeFormat_(negativeFormat)
+		{}
+
+	/// Internal Storage of the currency value, in cents
+	int cents;
+	/// Getter function for the currency value in dollars
+	float dollars(void) const;
+	/// Display the formatted dollar amount
+	std::string display(const int &max_characters = 10) const;
+	/// Comparison operator for two Currency objects
+	bool operator<(const Currency &rhs) const;
+	
+  private:	
+	std::string display_magnitude(const int &max_characters) const;
+	std::string format_negative_string(std::string positiveString) const;
+	const NegativeFormat negativeFormat_;
+};
+
+#endif
