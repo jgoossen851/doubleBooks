ebe644cc6172977d77850f0f14b853924d4ce3fc
Jeremy Goossen <jeremyg995@gmail.com>
2021-12-19 00:28:57 -0600

    Adds compile options, fixes packaging instruction, uninstalls Install Manifest, and add script to verify build procedure.

---
+++ verifyBuild.sh
@@ -0,0 +1,15 @@
+
+rm -rf build/         || (echo -e "\e[31mFailed to remove build dir\e[0m" && exit)
+mkdir build/          || (echo -e "\e[31mFailed to make build dir\e[0m" && exit)
+pushd build/          || (echo -e "\e[31mFailed to enter build dir\e[0m" && exit)
+
+cmake ..              || (echo -e "\e[31mFailed to generate Makefile\e[0m" && popd && exit)
+cmake --build .       || (echo -e "\e[31mFailed to build project\e[0m" && popd && exit)
+make docs             || (echo -e "\e[31mFailed to build documentation\e[0m" && popd && exit)
+make test             || (echo -e "\e[31mFailed to run tests\e[0m" && popd && exit)
+sudo make install     || (echo -e "\e[31mFailed to install executable\e[0m" && popd && exit)
+sudo make uninstall   || (echo -e "\e[31mFailed to uninstall executable\e[0m" && popd && exit)
+make package          || (echo -e "\e[31mFailed to package binary\e[0m" && popd && exit)
+make package_source   || (echo -e "\e[31mFailed to package source\e[0m" && popd && exit)
+
+popd
