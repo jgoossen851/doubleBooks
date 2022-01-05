f6899c1fbbbf712481dd762ca4862c12f3fec3d2
Jeremy Goossen <jeremyg995@gmail.com>
2022-01-05 12:34:36 -0600

    Implements a very basic user interface with switch statement in main.

--- src-trees/app/src/main.cpp
+++ src-trees/app/src/main.cpp
@@ -11,7 +11,30 @@
 #include "register.h"
 
 #include <iostream>
-#include <cstdlib>
+#include <map>
+#include <functional>
+#include <string>
+
+enum COMMANDS {
+  LOAD,
+  PRINT
+};
+
+Register loadData(const char * filename){
+  // Set up files
+  Csv transactionsList(filename);
+  std::cout << "CSV opened." << std::endl;
+
+  // Get installed location of configuration file
+  char *env_home;
+  env_home = getenv("HOME");
+  std::string pathStr = env_home;
+  pathStr.append("/.local/share/doublebooks/Accounts.dbkcfg");
+
+  // Load database
+  std::cout << "Database loaded." << std::endl;
+  return Register(transactionsList.load(), pathStr.c_str());
+}
 
 int main(int argc, char* argv[]) {
 
@@ -22,30 +45,31 @@ int main(int argc, char* argv[]) {
     return 1;
   }
   
+  // Define user commands
+  std::map< std::string, COMMANDS > dictionary;
+  dictionary["l"] = LOAD;
+  dictionary["p"] = PRINT;
+
   // Initialize state variables
   Register accountRegister;
 
-  // Reload data
-  {
-    // Set up files
-    Csv transactionsList(argv[1]);
-    std::cout << "CSV opened." << std::endl;
-
-    // Get installed location of configuration file
-    char *env_home;
-    env_home = getenv("HOME");
-    std::string pathStr = env_home;
-    pathStr.append("/.local/share/doublebooks/Accounts.dbkcfg");
-
-    // Load database
-    accountRegister = Register(transactionsList.load(), pathStr.c_str());
-    std::cout << "Database loaded." << std::endl;
-  }
-    
-  // Reprint output
-  {
-    // Print Table
-    accountRegister.printSplits();
+  std::string input;
+  while(std::getline(std::cin, input)) { // quit the program with Ctrl+D (EOF) or Ctrl+C (SIGINT)
+    auto itr = dictionary.find(input);
+    if ( itr != end(dictionary)) {
+      switch (itr->second) {
+        case LOAD : // Reload data
+          accountRegister = loadData(argv[1]);
+          break;
+        case PRINT : // Reprint output
+          accountRegister.printSplits();
+          break;
+        default :
+          std::cout << "Command \"" << input << "\" Not Implemented" << std::endl; 
+      }
+    } else {
+      std::cout << "command \"" << input << "\" not known" << std::endl;
+    }
   }
 
   return 0;
